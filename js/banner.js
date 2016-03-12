7079b2c5d0c8591fe3b991d6eced3601eb6e8c71
毛球 <392818909@qq.com>
2016-03-12 20:38:24 +0800

    bug fix

--- js/banner.js
+++ js/banner.js
@@ -1,107 +1,80 @@
-// 动态生成banner高度
+// 根据实际页面动态生成容器高度
 function bannerHeight(){
-    var banner = document.getElementById("banner");
-    var li = document.getElementById("list").getElementsByTagName("li")[0];
-    var liLen = window.getComputedStyle(li,null).getPropertyValue("height");
-    banner.style.height = liLen;
+    var img = imgArr[0];
+    var rightHeight = window.getComputedStyle(img).getPropertyValue('height');
+    banner.style.height = rightHeight;
 }
-window.onload = bannerHeight();
-// 判断obj是否有此class
-function hasClass(obj,cls){
-    return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+// 判断是否有cls类
+function hasClass(obj, cls){
+    var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+    return obj.className.match(reg);
 }
-// 给obj添加class
-function addClass(obj,cls){
-    if(!this.hasClass(obj,cls)){
+// 增加cls类
+function addClass(obj, cls){
+    if (!hasClass(obj,cls)){
         obj.className += cls;
     }
 }
-// 移除obj对应的class
-function removeClass(obj,cls){
-    if(hasClass(obj,cls)){
-        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
-        obj.className = obj.className.replace(reg,'');
-    }
+// 移除cls类
+function removeClass(obj, cls){
+    var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+    obj.className = obj.className.replace(reg, ''); // 这里有个小问题就是 如果是"aa bb cc".replace(" bb ", '')这种情况就会导致aa和cc之间没有空格
 }
-function setOpacity(elem,level){
-        elem.style.opacity = level / 100;
-}
-// 淡入处理函数
-function fadeIn(curImg, tarImg){
-    curImg.style.opacity = '0';
-    tarImg.style.opacity = '1';
-}
-//淡出处理函数
-// function fadeOut(elem){
-//     for(var i = 0; i <= 20; i++){
-//         (function(){
-//             var level = 100 - i * 5;
-//             setTimeout(function(){
-//                 setOpacity(elem,level);
-//             },i * 25);
-//         })(i);
-//     }
-// }
-// 变换处理函数
+// 改变图片和小圆点
 function changeTo(num){
-    var curImg = document.getElementsByClassName("show")[0];
-    //fadeOut(curImg);
-    removeClass(curImg, "show");
-    addClass(imgArr[num],"show");
-    fadeIn(curImg, imgArr[num]);
-    var _curIndex = document.getElementsByClassName("active")[0];
-    removeClass(_curIndex, "active");
-    addClass(indexArr[num], "active");
+    var curImg = document.getElementsByClassName('show')[0];
+    var tarImg = imgArr[num];
+    removeClass(curImg, 'show');
+    addClass(tarImg, 'show');
+    var curIndex = document.getElementsByClassName('active')[0];
+    var tarIndex = indexArr[num];
+    removeClass(curIndex, 'active');
+    addClass(tarIndex, 'active');
 }
-// index添加事件处理
-function addEvent(){
-    for(var i = 0; i < imgLen; i++){
+// 小圆点点击事件监听
+function indexClick(){
+    for (i = 0; i < imgLen; i++){
         (function(_i){
-            indexArr[_i].onmousedown = function(){
-                clearTimeout(autoChange);
+            indexArr[_i].addEventListener('click', function(){
                 changeTo(_i);
                 curIndex = _i;
-            };
-            // indexArr[_i].onmouseout = function(){
-            //     autoChange = setInterval(function(){
-            //         if(curIndex < imgLen - 1){
-            //             curIndex++;
-            //         }else{
-            //             curIndex = 0;
-            //         }
-            //         changeTo(curIndex);
-            //     },5000);
-            // };
-        })(i);
+            },false);
+        })(i)
     }
 }
-// 悬停暂停
+// 停止自动播放
 function stop(){
     clearInterval(autoChange);
 }
-var curIndex = 0;
-var banner = document.getElementById("banner");
-var imgArr = document.getElementById("list").getElementsByTagName("li");
-var imgLen = imgArr.length;
-var indexArr = document.getElementById("pointer").getElementsByTagName("li");
 
+
+var banner = document.getElementById('banner');
+var imgArr = document.getElementById('list').getElementsByTagName('li');
+var imgLen = imgArr.length;
+var indexArr = document.getElementById('pointer').getElementsByTagName('li');
+var curIndex = 0;
 var autoChange = setInterval(function(){
-    if(curIndex < imgLen - 1){
+    if (curIndex < imgLen - 1){
         curIndex++;
     }else{
         curIndex = 0;
     }
     changeTo(curIndex);
-},5000);
-addEvent();
-banner.addEventListener('mouseover', stop, false);
-banner.addEventListener('mouseout', function(){
-    autoChange = setInterval(function(){
-        if(curIndex < imgLen - 1){
-            curIndex++;
-        }else{
-            curIndex = 0;
-        }
-        changeTo(curIndex);
-    },5000);
-}, false);
+}, 5000);
+
+window.onload = function(){
+    bannerHeight();
+    indexClick();
+    banner.addEventListener('mouseover', stop, false);
+    banner.addEventListener('mouseout', function(){
+        autoChange = setInterval(function(){
+            if(curIndex < imgLen - 1){
+                curIndex++;
+            }else{
+                curIndex = 0;
+            }
+            changeTo(curIndex);
+        },5000);
+    }, false);
+    window.addEventListener('resize',bannerHeight,false);
+}
